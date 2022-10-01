794bd4d7002a0a4f678569ffc66fdc2725bc719d
mehtanishad <mehtanishad317@gmail.com>
2022-10-01 13:08:00 +0530

    fundamental of python

---
+++ factorial_number.py
@@ -0,0 +1,7 @@
+
+n= int(input("enter your number:"))
+factorial =1
+if n>= 1:
+  for i in range(1,n+1):
+    factorial= factorial*i
+    print ("Factorial of the given number is: ", factorial)
---
+++ number_positive_negative_zero.py
@@ -0,0 +1,9 @@
+# Write a Python program to check if a number is positive, negative or
+zero.
+n = float(input("Enter a number: "))
+if n> 0:
+   print("Positive number")
+elif n == 0:
+   print("Zero")
+else:
+   print("Negative number")
